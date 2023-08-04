b71124a36ae6303c5f1e9082c54baec1c9ff5fd0
Ishaan <cishaan303@gmail.com>
2023-08-05 02:27:10 +0530

    Integer to roman

---
+++ Day 31/Integer_to_roman.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        string Roman = "";
+       
+        vector<pair<int, string>> storeIntRoman = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400,
+         "CD"}, {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"},
+          {4, "IV"}, {1, "I"}};
+       
+        for (int i = 0; i < storeIntRoman.size(); i++) {
+            while (num >= storeIntRoman[i].first) {
+                Roman += storeIntRoman[i].second;
+                num -= storeIntRoman[i].first;
+            }
+        }
+        return Roman;
+    }
+};
+
+int main() {
+    Solution solution;
+
+    int num;
+    cout << "Enter an integer: ";
+    cin >> num;
+
+    string result = solution.intToRoman(num);
+    cout << "Roman numeral representation of " << num << " is: " << result << endl;
+    return 0;
+}
